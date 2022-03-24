0ab9615da5719ace2a3ef44bae877964a6d0da04
Fatih Yıldız <0m.fatih.yildiz@gmail.com>
2022-03-25 01:26:29 +0300

    fixed menuItem props

--- src/components/nav/menuItem.vue
+++ src/components/nav/menuItem.vue
@@ -4,13 +4,14 @@
             <slot></slot>
         </div>
         <TransitionRoot
-            :show="true"
-            enter="transition-opacity duration-75"
-            enter-from="opacity-0"
-            enter-to="opacity-100"
-            leave="transition-opacity duration-150"
-            leave-from="opacity-100"
-            leave-to="opacity-0"
+            appear
+            :show="props.notification"
+            enter="transition-all transform duration-150"
+            enter-from="opacity-0 scale-75"
+            enter-to="opacity-100 scale-100"
+            leave="transition-all transform duration-150"
+            leave-from="opacity-100 scale-100"
+            leave-to="opacity-0 scale-75"
         >
             <div class="notification">5</div>
         </TransitionRoot>
@@ -19,21 +20,22 @@
 
 <script setup>
 import { TransitionRoot } from '@headlessui/vue'
-import { ref, onMounted } from 'vue'
+import { ref } from 'vue'
 
 const props = defineProps({
     notification: Boolean
 });
 
-if (props.notification) {
-    console.log(props.notification)
-}
+
+// if (props.notification) {
+//     console.log(props.notification)
+// }
 
 </script>
 
 <style lang="scss">
 .item {
-    @apply flex justify-between items-center transition-colors duration-100 w-full px-4 py-2.5 rounded-lg text-p14 text-left capitalize hover:text-gray-300 active:text-gray-200 hover:bg-bn-gray-400/5 active:bg-bn-gray-400/10 select-none focus:outline-none focus:ring-2 focus:ring-bn-gray-800;
+    @apply flex text-gray-400 justify-between items-center transition-colors duration-100 w-full px-4 h-12 rounded-lg text-p14 text-left capitalize hover:text-gray-300 active:text-gray-200 hover:bg-bn-gray-400/5 active:bg-bn-gray-400/10 select-none focus:outline-none focus:ring-2 focus:ring-bn-gray-800;
     div {
         @apply flex space-x-4 items-center;
         svg {
@@ -44,7 +46,7 @@ if (props.notification) {
         }
     }
     .notification {
-        @apply bg-primary text-white px-2 py-1 text-xs rounded-md;
+        @apply bg-primary text-white px-2 py-1 text-xs rounded-md font-medium;
     }
 }
 </style>
--- src/components/nav/nav.vue
+++ src/components/nav/nav.vue
@@ -3,34 +3,64 @@
         <div class="content">
             <Logo />
             <div class="menu">
-                <MenuItem>
-                    <svg viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path
-                            d="M16 16L12.375 12.375M14.3333 7.66666C14.3333 11.3486 11.3486 14.3333 7.66666 14.3333C3.98476 14.3333 1 11.3486 1 7.66666C1 3.98476 3.98476 1 7.66666 1C11.3486 1 14.3333 3.98476 14.3333 7.66666Z"
-                            stroke="currentColor"
-                            stroke-width="1.5"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                        />
-                    </svg>
-                    <span>Ara</span>
-                </MenuItem>
-                <MenuItem :notification="notification">
-                    <svg
-                        viewBox="0 0 17 19"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path
-                            d="M9.94167 16.8333C9.79516 17.0859 9.58487 17.2955 9.33185 17.4413C9.07884 17.587 8.79198 17.6637 8.5 17.6637C8.20802 17.6637 7.92116 17.587 7.66814 17.4413C7.41513 17.2955 7.20484 17.0859 7.05833 16.8333M13.5 6C13.5 4.67392 12.9732 3.40215 12.0355 2.46447C11.0979 1.52678 9.82608 1 8.5 1C7.17392 1 5.90215 1.52678 4.96447 2.46447C4.02678 3.40215 3.5 4.67392 3.5 6C3.5 11.8333 1 13.5 1 13.5H16C16 13.5 13.5 11.8333 13.5 6Z"
-                            stroke="#B2B7BA"
-                            stroke-width="1.5"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                        />
-                    </svg>
-                    <span>Bildirim</span>
-                </MenuItem>
+                <div class>
+                    <div class="other">
+                        <MenuItem>
+                            <svg
+                                viewBox="0 0 17 17"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    d="M16 16L12.375 12.375M14.3333 7.66666C14.3333 11.3486 11.3486 14.3333 7.66666 14.3333C3.98476 14.3333 1 11.3486 1 7.66666C1 3.98476 3.98476 1 7.66666 1C11.3486 1 14.3333 3.98476 14.3333 7.66666Z"
+                                    stroke="currentColor"
+                                    stroke-width="1.5"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                />
+                            </svg>
+                            <span>Ara</span>
+                        </MenuItem>
+                        <MenuItem
+                            @click="notification = !notification"
+                            :notification="notification"
+                        >
+                            <svg
+                                viewBox="0 0 17 19"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    d="M9.94167 16.8333C9.79516 17.0859 9.58487 17.2955 9.33185 17.4413C9.07884 17.587 8.79198 17.6637 8.5 17.6637C8.20802 17.6637 7.92116 17.587 7.66814 17.4413C7.41513 17.2955 7.20484 17.0859 7.05833 16.8333M13.5 6C13.5 4.67392 12.9732 3.40215 12.0355 2.46447C11.0979 1.52678 9.82608 1 8.5 1C7.17392 1 5.90215 1.52678 4.96447 2.46447C4.02678 3.40215 3.5 4.67392 3.5 6C3.5 11.8333 1 13.5 1 13.5H16C16 13.5 13.5 11.8333 13.5 6Z"
+                                    stroke="#B2B7BA"
+                                    stroke-width="1.5"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                />
+                            </svg>
+                            <span>Bildirim</span>
+                        </MenuItem>
+                    </div>
+                    <div class="menuInfo">General</div>
+                    <div class="general">
+                        <MenuItem>
+                            <svg
+                                viewBox="0 0 17 17"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    d="M16 16L12.375 12.375M14.3333 7.66666C14.3333 11.3486 11.3486 14.3333 7.66666 14.3333C3.98476 14.3333 1 11.3486 1 7.66666C1 3.98476 3.98476 1 7.66666 1C11.3486 1 14.3333 3.98476 14.3333 7.66666Z"
+                                    stroke="currentColor"
+                                    stroke-width="1.5"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                />
+                            </svg>
+                            <span>Ara</span>
+                        </MenuItem>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -49,7 +79,7 @@ const logo = ref({
 provide('logo', logo.value)
 
 // Notification Configure
-const notification = ref('true');
+const notification = ref(true);
 
 </script>
 
@@ -59,7 +89,13 @@ const notification = ref('true');
     .content {
         @apply h-full space-y-7 flex flex-col items-start;
         .menu {
-            @apply h-full w-full pr-10 space-y-2;
+            @apply h-full w-full pr-8 space-y-2;
+            .menuInfo {
+                @apply text-l12 px-4 font-bold font-medium text-gray-500 mt-3 mb-2;
+            }
+            .general, .other {
+                @apply space-y-2;
+            }
         }
     }
 }
