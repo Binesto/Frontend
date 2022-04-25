639f7fa79059af9c33cd464103af9b6205ee707b
Fatih Yıldız <0m.fatih.yildiz@gmail.com>
2022-04-26 00:18:56 +0300

    Refactored in mStatistic component

--- src/components/home/mStatistic.vue
+++ src/components/home/mStatistic.vue
@@ -1,20 +1,18 @@
 <template>
     <div class="main">
         <div class="mstat">
-            <div class="absolute left-0 h-full bg-gradient-to-l from-transparent to-dark-100 w-full z-10"></div>
-            <div class="space-y-2 z-10">
-                <span class="font-bold text-bn-gray-400 text-sm">Eth</span>
-                <div class="text-2xl font-bold">
-                    <span class="text-bn-gray-600">₺</span>54,067.75
+            <div class="mstat__overlay"></div>
+            <div class="mstat__content">
+                <span class="mstat__content__title">Eth</span>
+                <div class="mstat__content__detail">
+                    <span class="mstat__content__detail__unit">₺</span>54,067.75
                 </div>
-                <span
-                    class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-bn-red/20 text-bn-red tracking-wide"
-                >-%2.17</span>
+                <span class="mstat__content__percentile rise">%2.17</span>
             </div>
             <staticChart />
         </div>
         <div class="mdetail">
-            <span class="font-bold text-bn-gray-600 text-sm pt-9 pl-8">Detay</span>
+            <span class="mdetail__title">Detay</span>
             <mDetail />
         </div>
     </div>
@@ -35,9 +33,36 @@ import mDetail from './mDetail.vue'
     }
     .mstat {
         @apply justify-between items-center overflow-hidden p-8;
+        &__overlay{
+            @apply absolute left-0 h-full bg-gradient-to-l from-transparent to-dark-100 w-full z-10
+        }
+        &__content{
+            @apply space-y-2 z-10;
+            &__title{
+                @apply font-bold text-bn-gray-400 text-sm;
+            }
+            &__detail{
+                @apply text-2xl font-bold;
+                &__unit{
+                    @apply text-bn-gray-600;
+                }
+            }
+            &__percentile{
+                @apply inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium tracking-wide;
+            }
+            .drop{
+                @apply bg-bn-red/20 text-bn-red before:content-['-'];
+            }
+            .rise{
+                @apply bg-bn-green/20 text-bn-green;
+            }
+        }
     }
     .mdetail {
         @apply flex-col pb-8;
+        &__title {
+            @apply font-bold text-bn-gray-600 text-sm pt-9 pl-8;
+        }
     }
 }
 </style>
