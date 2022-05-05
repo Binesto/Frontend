f279c6aa2f7c28a4312ebc43a8153c49c9d70e91
Fatih Yıldız <0m.fatih.yildiz@gmail.com>
2022-05-06 00:56:34 +0300

    orderActivity component list side added

--- src/components/home/orderActivity.vue
+++ src/components/home/orderActivity.vue
@@ -13,41 +13,128 @@
           {{ tab.name }}
         </a>
         <div class="order__tab__nav__indicator">
-          <div class="order__tab__nav__indicator__ind" :class="currentData.id == 1 ? 'mx-2 left-0 translate-x-0' : currentData.id == 2 ? '-mx-2 left-1/2 -translate-x-1/2' : '-mx-6 left-full -translate-x-full'"></div>
+          <div
+            class="order__tab__nav__indicator__ind"
+            :class="
+              currentData.id == 1
+                ? 'mx-2 left-0 translate-x-0'
+                : currentData.id == 2
+                ? '-mx-2 left-1/2 -translate-x-1/2'
+                : '-mx-6 left-full -translate-x-full'
+            "
+          ></div>
         </div>
       </nav>
     </div>
     <div class="order__content">
-      <div v-if="isEmpty" class="order__content__empty">
+      <TransitionRoot
+        :show="isEmpty"
+        appear
+        as="div"
+        class="order__content__empty"
+        enter="transform transition duration-[400ms]"
+        enter-from="opacity-0 scale-95 w-0 h-0"
+        enter-to="opacity-100 scale-100"
+        leave="transform duration-200 transition ease-in-out"
+        leave-from="opacity-100 scale-100 "
+        leave-to="opacity-0 scale-95"
+      >
         <div class="order__content__empty__alert">
-          <img
-            src="@/assets/emptyAction.svg"
-            draggable="false"
-          />
+          <img src="@/assets/emptyAction.svg" draggable="false" />
           <h3 class="order__content__empty__alert__title">Bulunamadı...</h3>
           <p class="order__content__empty__alert__description">
             {{ currentData.emptyMessage }}
           </p>
         </div>
-      </div>
-      <div v-else>
-        <span v-for="(data, index) in currentData.data" :key="index">{{
-          data
-        }}</span>
-      </div>
+      </TransitionRoot>
+      <TransitionRoot
+        :show="!isEmpty"
+        appear
+        as="div"
+        class="order__content__empty z-10"
+        enter="transition duration-[400ms]"
+        enter-from="opacity-0 w-0 h-0"
+        enter-to="opacity-100"
+        leave="duration-200 transition ease-in-out"
+        leave-from="opacity-100 "
+        leave-to="opacity-0"
+      >
+        <table class="table">
+          <thead class="table__head">
+            <tr class="table__head__wrap">
+              <th scope="col">İşlem</th>
+              <th scope="col">Gerçekleşme Zamanı</th>
+              <th scope="col">Miktar(BTC)</th>
+              <th scope="col">Fiyat(TL)</th>
+              <th scope="col">Toplam(TL)</th>
+            </tr>
+          </thead>
+          <tbody class="table__body">
+            <tr class="table__body__wrap">
+              <td class="table__body__wrap__content">
+                <span
+                  class="table__body__wrap__content__status" status="rise"
+                >
+                  Alış
+                </span>
+              </td>
+              <td class="table__body__wrap__content">
+                00:42:42
+              </td>
+              <td class="table__body__wrap__content">
+                11,795.70
+                <span class="table__body__wrap__content__unit">₺</span>
+              </td>
+              <td class="table__body__wrap__content">
+                11,795.70
+                <span class="table__body__wrap__content__unit">BTC</span>
+              </td>
+              <td class="table__body__wrap__content">
+                11,795.70
+                <span class="table__body__wrap__content__unit">₺</span>
+              </td>
+            </tr>
+            <tr class="table__body__wrap">
+              <td class="table__body__wrap__content">
+                <span
+                  class="table__body__wrap__content__status" status="drop"
+                >
+                  Satış
+                </span>
+              </td>
+              <td class="table__body__wrap__content">
+                00:42:42
+              </td>
+              <td class="table__body__wrap__content">
+                11,795.70
+                <span class="table__body__wrap__content__unit">₺</span>
+              </td>
+              <td class="table__body__wrap__content">
+                11,795.70
+                <span class="table__body__wrap__content__unit">BTC</span>
+              </td>
+              <td class="table__body__wrap__content">
+                11,795.70
+                <span class="table__body__wrap__content__unit">₺</span>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </TransitionRoot>
     </div>
   </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
+import { TransitionRoot } from "@headlessui/vue";
 
 const tabs = ref([
   {
     id: 1,
     name: "Açık Emirlerim",
     current: true,
-    data: [],
+    data: [1],
     emptyMessage: "Henüz herhangi bir Açık Emriniz bulunmamakta.",
   },
   {
@@ -69,7 +156,7 @@ const tabs = ref([
 const isEmpty = ref(false);
 const currentData = ref([]);
 
-const setCurrent = (index,id) => {
+const setCurrent = (index, id) => {
   tabs.value.forEach((tab, i) => {
     if (i === index) {
       currentData.value = tab;
@@ -82,8 +169,7 @@ const setCurrent = (index,id) => {
     }
   });
 };
-setCurrent(0,1);
-
+setCurrent(0, 1);
 </script>
 
 <style lang="scss" scoped>
@@ -94,7 +180,7 @@ setCurrent(0,1);
     &__nav {
       @apply -mb-px flex px-4 py-2.5 space-x-2 max-w-max relative;
       &__item {
-        @apply w-44 text-center select-none rounded-xl transition-all duration-200 border-b-0 border-transparent hover:bg-bn-gray-800/5 px-8 py-3 cursor-pointer text-white/50 whitespace-nowrap font-medium text-xs;
+        @apply w-44 text-center select-none rounded-xl transition-all duration-200 border-b-0 border-transparent hover:bg-bn-gray-700/5 px-8 py-3 cursor-pointer text-white/50 whitespace-nowrap font-medium text-xs;
       }
       &__item.active {
         @apply transition-all duration-200 text-primary/80 shadow-lg;
@@ -102,25 +188,58 @@ setCurrent(0,1);
       &__indicator {
         @apply absolute top-1/2 left-1/2 -translate-x-1/2 transform  -translate-y-1/2 w-full h-full rounded-xl pointer-events-none;
         &__ind {
-          @apply transition-all duration-200 bg-primary/5 rounded-xl absolute top-1/2 transform -translate-y-1/2 h-4/6 w-44 ;
+          @apply transition-all duration-200 bg-primary/5 rounded-xl absolute top-1/2 transform -translate-y-1/2 h-4/6 w-44;
         }
       }
     }
   }
-  &__content{
-    @apply w-full h-72 flex;
-    &__empty{
-      @apply w-full h-full flex justify-center select-none;
-      &__alert{
+  &__content {
+    @apply w-full h-72 flex relative;
+    &__empty {
+      @apply w-full h-full flex justify-center select-none absolute inset-0;
+      &__alert {
         @apply text-center mt-12 w-full flex items-center flex-col;
-        img{
+        img {
           @apply -mt-6 select-none;
         }
-        &__title{
+        &__title {
           @apply mt-2.5 text-sm font-medium text-white;
         }
-        &__description{
-          @apply mt-1 text-bn-gray-700 text-xs font-medium;
+        &__description {
+          @apply mt-1 text-bn-gray-600 text-xs font-medium;
+        }
+      }
+    }
+    .table {
+      @apply w-full flex flex-col;
+      &__head {
+        @apply pb-1 w-full flex flex-col sticky top-0 z-20;
+        &__wrap {
+          @apply flex bg-[#141414] overflow-hidden;
+          th {
+            @apply flex w-full py-2.5 pl-4 pr-3 text-left text-xs font-semibold text-bn-gray-500 sm:pl-6 lg:pl-8;
+          }
+        }
+      }
+      &__body {
+        @apply space-y-1 h-full;
+        &__wrap {
+          @apply flex relative cursor-pointer hover:bg-bn-gray-900/50;
+          &__content {
+            @apply z-10 flex items-center w-full whitespace-nowrap py-3 pl-4 pr-3 text-xs font-medium text-bn-gray-200 sm:pl-6 lg:pl-8;
+            &__unit {
+              @apply text-bn-gray-600 ml-1;
+            }
+            &__status {
+              @apply inline-flex items-center px-4 py-1 rounded-md text-xs font-medium w-16 justify-center;
+              &[status="drop"] {
+                @apply bg-bn-red/5 text-bn-red;
+              }
+              &[status="rise"] {
+                @apply bg-bn-green/5 text-bn-green;
+              }
+            }
+          }
         }
       }
     }
